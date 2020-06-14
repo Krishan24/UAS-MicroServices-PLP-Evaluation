5f779da6aa30971510479b01c57260c25e5328f7
pavani2312 <61940479+pavani2312@users.noreply.github.com>
2020-06-14 21:30:41 +0530

    Update ExceptionController.java

--- ParticipantService/src/main/java/com/cg/uas/participantservice/exception/ExceptionController.java
+++ ParticipantService/src/main/java/com/cg/uas/participantservice/exception/ExceptionController.java
@@ -21,39 +21,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionController 
 {
-	/************************************************************************************
-	 - Method Name      : handleNameException
-	 - Return type      : String
-	 - Author           : SatyaPavani Chekka
-	 - Creation Date    : 11-06-2020
-	 - Description      : To check the ProgramName Exception
-	************************************************************************************/
+	
 	@ExceptionHandler(ProgramNameNotFoundException.class)
 	public String handleNameException(ProgramNameNotFoundException e) {
 		
 		return e.getMessage();
 	}
 	
-	/************************************************************************************
-	 - Method Name      : handleException
-	 - Return type      : String
-	 - Author           : SatyaPavani Chekka
-	 - Creation Date    : 11-06-2020
-	 - Description      : To check whether application ID is there are not there Exception
-	************************************************************************************/
+	
 	@ExceptionHandler(ApplicationIdNotFoundException.class)
 	public String handleException(ApplicationIdNotFoundException e) {
 		
 		return e.getMessage();
 	}
 	
-	/************************************************************************************
-	 - Method Name      : handleConstraintViolation
-	 - Return type      : String
-	 - Author           : SatyaPavani Chekka
-	 - Creation Date    : 11-06-2020
-	 - Description      : To check Constraint Exception
-	************************************************************************************/
+	
 	@ExceptionHandler(ConstraintViolationException.class)
 	public Map<String, String> handleConstraintViolation(ConstraintViolationException ex) {
 	    Map<String, String> errors = new HashMap<>();
@@ -66,18 +48,6 @@ public class ExceptionController
 	    return errors;
 	}
 	
-	
-	/************************************************************************************
-	 - Method Name      : handleMethodArgumentNotValidException
-	 - Return type      : String
-	 - Author           : SatyaPavani Chekka
-	 - Creation Date    : 11-06-2020
-	 - Description      : To check the Methods exceptions
-	************************************************************************************/
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	  @ResponseStatus(HttpStatus.BAD_REQUEST)
-	  ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-	    return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
-	  }
+
 
 }
