a932501d1ce2df7541bd1382282cc368f8ac6f52
Krishan Sitlani <61962073+Krishan24@users.noreply.github.com>
2020-06-15 14:05:04 +0530

    Entity Updated

--- ScheduleProgramService/src/main/java/com/cg/uas/schedule/program/service/dto/ScheduleProgram.java
+++ ScheduleProgramService/src/main/java/com/cg/uas/schedule/program/service/dto/ScheduleProgram.java
@@ -11,8 +11,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -22,21 +25,23 @@ import org.springframework.format.annotation.DateTimeFormat;
 public class ScheduleProgram 
 {
 	@Id
+	@Min(value=1, message="Id cannot be smaller than 1")
+	@Max(value=999,message="ID cannot be more than 3 digit")
 	@NotNull(message = "Program Id  should no be null")
 	@Column(name="program_id")
+	@Positive(message="program id should be positive")
 	private Integer programId;
 	
 	@NotBlank(message = "Program Name should not be blank")
 	@NotNull(message = "Program Name should not be null")
 	@Column(name="program_name")
-	@Size(min=2,max=8)
+	@Size(min=2,max=10, message="Program name should be inside 2 to 10 characters")
 	private String programName;
 	
 	@NotBlank(message = "Location should no be blank")
-	@NotNull(message = "Location should not be null")
 	@Size(min=2,max=8)
+	@NotNull(message = "Location should not be null")
 	private String location;
-	
 	@DateTimeFormat(pattern= "yyyy-mm-dd")	
 	@Column(name="start_date")
 	@NotNull(message = "Start Date should not be null")
@@ -48,6 +53,8 @@ public class ScheduleProgram
 	private Date endDate;
 	
 	@Column(name="no_of_seats")
+	@Positive(message="No Of Seats should be positive")
+	@Max(value= 200 ,message="No of seats maximum should be 200")
 	@NotNull(message = "No Of Seats should not be null")
 	private Integer noOfSeats;
 	
